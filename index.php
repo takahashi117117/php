31de5f73acf24c0ff3f9a313dcb6c1cab924ec58
webox-de <info@webox-de.com>
2020-01-17 16:36:09 +0900

    初期状態

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP教材01</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+</body>
+</html>
